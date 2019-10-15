87bb42fbb082078a34406fae9160812bc108284b
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-10-15 15:54:47 -0400

    extend vertical rule to bottom of modal

--- frontend/src/metabase/visualizations/components/ChartSettings.jsx
+++ frontend/src/metabase/visualizations/components/ChartSettings.jsx
@@ -227,7 +227,7 @@ class ChartSettings extends Component {
 
     // default layout with visualization
     return (
-      <div>
+      <div className="flex flex-column spread">
         {sectionNames.length > 1 && (
           <div className="flex flex-no-shrink pl4 pt2 pb1">{sectionPicker}</div>
         )}
@@ -236,7 +236,7 @@ class ChartSettings extends Component {
             {widgetList}
           </div>
         ) : (
-          <div className="Grid">
+          <div className="Grid flex-full">
             <div className="Grid-cell Cell--1of3 scroll-y scroll-show border-right py4">
               {widgetList}
             </div>
